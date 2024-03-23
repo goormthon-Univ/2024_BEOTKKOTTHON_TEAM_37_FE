e734d437f02ad3c22bb58fcc98ef102b0fba6ab2
taehwanlee203 <teein5579@naver.com>
2024-03-24 04:25:26 +0900

    [feat] about 페이지 추가

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 jQuery(function ($) {
-	$(".menu_toggle_btn").click(function () {
-		$('.gnb').stop().slideToggle('fast');
-	});
+    $(".menu_toggle_btn").click(function () {
+        $('.gnb').stop().slideToggle('fast');
+        var otherButton = $("#otherButton");
+        // 다른 버튼의 마진을 변경하는 로직
+        if (otherButton.css("margin-top") === "200px") {
+            otherButton.animate({ marginTop: '50px' }, 'fast'); // 부드럽게 변경
+        } else {
+            otherButton.animate({ marginTop: '200px' }, 'fast'); // 부드럽게 변경
+        }
+    });
 });
