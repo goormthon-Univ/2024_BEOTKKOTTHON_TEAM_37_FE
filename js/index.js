854f3129ce856eeee4d2567c356e2ee4aee665b1
taehwanlee203 <teein5579@naver.com>
2024-03-24 06:46:07 +0900

    [feat] main startnow 재수정

--- js/index.js
+++ js/index.js
@@ -3,10 +3,10 @@ jQuery(function ($) {
         $('.gnb').stop().slideToggle('fast');
         var otherButton = $("#otherButton");
         // 다른 버튼의 마진을 변경하는 로직
-        if (otherButton.css("margin-top") === "130px") {
+        if (otherButton.css("margin-top") === "160px") {
             otherButton.animate({ marginTop: '50px' }, 'fast'); // 부드럽게 변경
         } else {
-            otherButton.animate({ marginTop: '130px' }, 'fast'); // 부드럽게 변경
+            otherButton.animate({ marginTop: '160px' }, 'fast'); // 부드럽게 변경
         }
     });
 });
